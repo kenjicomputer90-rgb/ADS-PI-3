c7b864be24b3164b78981fd515d494432fb6a48b
kenjicomputer90-rgb <kenjicomputer90@gmail.com>
2026-09-04 13:24:47 -0300

    first commit

---
+++ loja_roupas/src/main/java/com/loja_roupas/App.java
@@ -0,0 +1,13 @@
+package com.loja_roupas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
